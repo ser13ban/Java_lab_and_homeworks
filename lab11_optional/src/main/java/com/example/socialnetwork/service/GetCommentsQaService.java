89c3e21d34017c4f04f30fb35d8d6f228b211eb5
Teodorovici Gavril <teodorovici.gavril11@gmail.com>
2021-05-21 20:19:43 +0300

    language verification

--- lab11_optional/src/main/java/com/example/socialnetwork/service/GetCommentsQaService.java
+++ lab11_optional/src/main/java/com/example/socialnetwork/service/GetCommentsQaService.java
@@ -5,20 +5,25 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GetCommentsQaService {
     public List<Comment> qaComments(String Id, String token) throws Exception {
         GetCommentsService commentsService = new GetCommentsService();
         List<Comment> commentList = commentsService.call_me_to_get_comments(Id,token);
         commentList = filterQuestionMark(commentList);
-        //commentList = filterVulgarity(commentList);
+        commentList = filterVulgarity(commentList);
         return commentList;
     }
 
@@ -33,10 +38,26 @@ public class GetCommentsQaService {
         return filteredList;
     }
 
-    public List<Comment> filterVulgarity(List<Comment> commentList){
-        //TODO filter the comments list for question that have clean language
+    public Boolean hasCurseWord(String sentence){
+        String fileName = "badwods.txt";
+        List<String> curseWords = new ArrayList<>();
 
-        return null;
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(fileName))) {
+            curseWords = br.lines().collect(Collectors.toList());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String curseWord: curseWords) {
+            if(sentence.contains(curseWord))
+                return false;
+        }
+        return true;
+    }
+
+    public List<Comment> filterVulgarity(List<Comment> commentList){
+        return commentList.stream().filter(c -> hasCurseWord(c.getMessage())).collect(Collectors.toList());
     }
     public Comment randomComment(List<Comment> commentList){
         do{
