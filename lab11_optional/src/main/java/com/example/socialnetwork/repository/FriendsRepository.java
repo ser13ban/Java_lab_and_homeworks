c90b73d3a017c315e9e7aa1d35fde2c2e1005b8f
Serban Mihai <ser13ban@gmail.com>
2021-05-22 19:48:46 +0300

    removed unused code

--- lab11_optional/src/main/java/com/example/socialnetwork/repository/FriendsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.socialnetwork.repository;
-
-import com.example.socialnetwork.entity.Friends;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FriendsRepository extends JpaRepository<Friends, Integer> {
-}
